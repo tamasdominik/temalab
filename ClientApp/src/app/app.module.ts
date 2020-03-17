3cab6d87b5bcc7c736a3096169097d775e2426b7
Tomi <csktomi@gmail.com>
2020-03-17 17:57:18 +0100

    BetaMilestones

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { ProfileComponent } from './profile/profile.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
+import { MilestonesComponent } from './milestones/milestones.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { WorkoutsComponent } from './workouts/workouts.component';
     CounterComponent,
     FetchDataComponent,
     ProfileComponent,
-    WorkoutsComponent
+    WorkoutsComponent,
+    MilestonesComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -36,6 +38,7 @@ import { WorkoutsComponent } from './workouts/workouts.component';
       { path: 'profile', component: ProfileComponent },
       { path: 'workouts', component: WorkoutsComponent },
       { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
+      { path: 'milestones', component: MilestonesComponent},
     ])
   ],
   providers: [
