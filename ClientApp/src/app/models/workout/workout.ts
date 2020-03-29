149bb85207fb4b25132b7a0210174f55534a890b
Nóra Zanati <zanati.nora@gmail.com>
2020-03-29 20:14:57 +0200

    workout,excercise model fix

--- ClientApp/src/app/models/workout/workout.ts
+++ ClientApp/src/app/models/workout/workout.ts
@@ -2,8 +2,5 @@ import { Exercise } from "../Exercise/exercise";
 
 export class Workout {
     workoutName : string;
-    exerciseName : string;
-    difficulty : number;
-    set : number;
-    reps : number;
+    exercise : Exercise;
 }
