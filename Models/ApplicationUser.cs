0a513de0f8ec980ee4cd687b02d6b3be4f077671
ferenc dávid <ferkodavid5@gmail.com>
2020-04-23 10:17:20 +0200

    nem mukodik

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -8,6 +8,9 @@ namespace Temalab_Fitness.Models
 {
     public class ApplicationUser : IdentityUser
     {
-
+        public char Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
     }
 }
