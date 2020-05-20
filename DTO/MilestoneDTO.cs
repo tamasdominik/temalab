d5ae38fe92fdf0d45a6d744cb4b133c181f8e432
Dominik Tamás <tamasdominikmate@outlook.com>
2020-05-20 18:51:25 +0200

    hotfix

---
+++ DTO/MilestoneDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Temalab_Fitness.DTO
+{
+    public class MilestoneDTO
+    {
+        public string Name { get; set; }
+        public int Counter { get; set; }
+        public int Goal { get; set; }
+        public MilestoneDTO()
+        {
+
+        }
+        public MilestoneDTO(string name, int cnt, int goal)
+        {
+            Name = name;
+            Counter = cnt;
+            Goal = goal;
+        }
+        public void AddCounter(int x)
+        {
+            Counter += x;
+        }
+    }
+}
